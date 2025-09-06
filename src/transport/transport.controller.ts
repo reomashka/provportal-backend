b51e988976eed17eb1584730452722e32ad54eaf
reomashka <romankuvaev820@gmail.com>
2025-09-06 14:22:19 +0300

    fixes

--- src/transport/transport.controller.ts
+++ src/transport/transport.controller.ts
@@ -1,23 +1,30 @@
-import { Controller, Get, Param, ParseIntPipe, Query, Request } from '@nestjs/common';
-import { TransportService } from './transport.service';
-import { TransportQuery } from './transport.service';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+  Request,
+} from "@nestjs/common";
+import { TransportService } from "./transport.service";
+import { TransportQuery } from "./transport.service";
 
-@Controller('transport')
+@Controller("transport")
 export class TransportController {
   constructor(private readonly transportService: TransportService) {}
 
-  @Get('get-all')
+  @Get("get-all")
   async getAll(@Query() query: TransportQuery) {
     return this.transportService.getAll(query);
   }
 
-  @Get('get-one/:id')
-  async getOneTransport(@Param('id', ParseIntPipe) id: number) {
+  @Get("get-one/:id")
+  async getOneTransport(@Param("id", ParseIntPipe) id: number) {
     return this.transportService.getOne(id);
   }
 
-  @Get('likes/:id')
-  async getUserLikedProducts(@Param('id') id: string) {
-    return this.transportService.getLikedTransport(Number(id));
-  }
+  // @Get('likes/:id')
+  // async getUserLikedProducts(@Param('id') id: string) {
+  //   return this.transportService.getLikedTransport(Number(id));
+  // }
 }
--- src/transport/transport.service.ts
+++ src/transport/transport.service.ts
@@ -1,16 +1,10 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "@/prisma/prisma.service";
+import { TransportClass } from "@prisma/client";
 
 export interface TransportQuery {
   order?: "asc" | "desc";
-  class?:
-    | "moto"
-    | "passenger"
-    | "cargo"
-    | "public"
-    | "container"
-    | "exclusive"
-    | "fraction";
+  class?: TransportClass;
 }
 
 @Injectable()
@@ -21,7 +15,7 @@ export class TransportService {
     const transport = await this.prisma.transport.findUnique({
       where: { id },
     });
-    if (!transport || transport.class === "fraction") {
+    if (!transport || transport.class === TransportClass.FRACTION) {
       throw new NotFoundException(
         `Transport with ID ${id} not found or it is fraction transport!`
       );
@@ -41,18 +35,5 @@ export class TransportService {
         price: order,
       },
     });
-  }
-
-  async getLikedTransport(userId: number) {
-    const user = await this.prisma.user.findUnique({
-      where: { id: userId },
-      include: { likes: { include: { transport: true } } },
-    });
-
-    if (!user) {
-      throw new NotFoundException("Пользователь не найден");
-    }
-
-    return user;
   }
 }
