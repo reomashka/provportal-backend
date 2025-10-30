13b835de9dff388ed3776049aaf2bdea92b50d8a
reomashka <romankuvaev820@gmail.com>
2025-10-30 19:39:32 +0300

    feat: new transport class in endpoint of transport (GET)

--- src/transport/transport.service.ts
+++ src/transport/transport.service.ts
@@ -3,50 +3,53 @@ import { PrismaService } from "@/prisma/prisma.service";
 import { TransportClass } from "@prisma/client";
 
 export interface TransportQuery {
-  order?: "asc" | "desc";
-  class?: TransportClass;
+    order?: "asc" | "desc";
+    class?: TransportClass;
 }
 
 @Injectable()
 export class TransportService {
-  constructor(private readonly prisma: PrismaService) {}
+    constructor(private readonly prisma: PrismaService) {}
 
-  public async getCurrentTransport(id: number) {
-    const transport = await this.prisma.transport.findUnique({
-      where: { id },
-    });
+    public async getCurrentTransport(id: number) {
+        const transport = await this.prisma.transport.findUnique({
+            where: { id },
+        });
 
-    if (!transport || transport.class === TransportClass.FRACTION) {
-      throw new NotFoundException(
-        `Transport with ID ${id} not found or it is fraction transport!`
-      );
+        if (!transport || transport.class === TransportClass.FRACTION) {
+            throw new NotFoundException(
+                `Transport with ID ${id} not found or it is fraction transport!`
+            );
+        }
+
+        return transport;
     }
 
-    return transport;
-  }
+    private readonly classMap: Record<
+        "moto" | "passenger" | "cargo" | "public" | "container" | "exclusive",
+        TransportClass
+    > = {
+        passenger: TransportClass.PASSENGER,
+        cargo: TransportClass.CARGO,
+        public: TransportClass.PUBLIC,
+        container: TransportClass.CONTAINER,
+        exclusive: TransportClass.EXCLUSIVE,
+        moto: TransportClass.MOTO,
+    };
 
-  private readonly classMap: Record<
-    "passenger" | "cargo" | "public",
-    TransportClass
-  > = {
-    passenger: TransportClass.PASSENGER,
-    cargo: TransportClass.CARGO,
-    public: TransportClass.PUBLIC,
-  };
+    public async getAll(query: TransportQuery = {}) {
+        const order = query.order || "asc";
+        const transportClass = query.class
+            ? this.classMap[query.class as keyof typeof this.classMap]
+            : undefined;
 
-  public async getAll(query: TransportQuery = {}) {
-    const order = query.order || "asc";
-    const transportClass = query.class
-      ? this.classMap[query.class as keyof typeof this.classMap]
-      : undefined;
-
-    return this.prisma.transport.findMany({
-      where: {
-        class: transportClass,
-      },
-      orderBy: {
-        price: order,
-      },
-    });
-  }
+        return this.prisma.transport.findMany({
+            where: {
+                class: transportClass,
+            },
+            orderBy: {
+                price: order,
+            },
+        });
+    }
 }
